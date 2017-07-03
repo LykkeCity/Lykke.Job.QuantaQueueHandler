588cd369544ef93945c34b3eb33fbe2852b9bd22
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2017-07-03 17:57:23 +0300

    Health monitoring added

--- src/Lykke.Job.QuantaQueueHandler/TriggerHandlers/QuantaQueueHandler.cs
+++ src/Lykke.Job.QuantaQueueHandler/TriggerHandlers/QuantaQueueHandler.cs
@@ -6,6 +6,7 @@ using Lykke.Job.QuantaQueueHandler.Contract;
 using Lykke.Job.QuantaQueueHandler.Core;
 using Lykke.Job.QuantaQueueHandler.Core.Domain.BitCoin;
 using Lykke.Job.QuantaQueueHandler.Core.Domain.PaymentSystems;
+using Lykke.Job.QuantaQueueHandler.Core.Services;
 using Lykke.Job.QuantaQueueHandler.Services.Exchange;
 using Lykke.JobTriggers.Triggers.Attributes;
 
@@ -18,16 +19,18 @@ namespace Lykke.Job.QuantaQueueHandler.TriggerHandlers
         private readonly ExchangeOperationsService _exchangeOperationsService;
         private readonly IPaymentSystemsRawLog _paymentSystemsRawLog;
         private readonly IPaymentTransactionsRepository _paymentTransactionsRepository;
+        private readonly IHealthService _healthService;
 
         public QuantaQueueHandler(IWalletCredentialsRepository walletCredentialsRepository, ILog log,
             ExchangeOperationsService exchangeOperationsService, IPaymentSystemsRawLog paymentSystemsRawLog,
-            IPaymentTransactionsRepository paymentTransactionsRepository)
+            IPaymentTransactionsRepository paymentTransactionsRepository, IHealthService healthService)
         {
             _walletCredentialsRepository = walletCredentialsRepository;
             _log = log;
             _exchangeOperationsService = exchangeOperationsService;
             _paymentSystemsRawLog = paymentSystemsRawLog;
             _paymentTransactionsRepository = paymentTransactionsRepository;
+            _healthService = healthService;
         }
 
         [QueueTrigger("quanta-in")]
@@ -38,19 +41,19 @@ namespace Lykke.Job.QuantaQueueHandler.TriggerHandlers
 
             try
             {
+                _healthService.TraceMessageProcessingStarted();
+
                 var walletCreds = await _walletCredentialsRepository.GetByQuantaContractAsync(msg.Contract);
                 if (walletCreds == null)
                 {
-                    await
-                        _log.WriteWarningAsync(nameof(QuantaQueueHandler), nameof(ProcessInMessage), msg.ToJson(),
-                            "Wallet not found");
+                    await _log.WriteWarningAsync(nameof(QuantaQueueHandler), nameof(ProcessInMessage), msg.ToJson(), "Wallet not found");
+
+                    _healthService.TraceMessageProcessingFailed();
+
                     return;
                 }
 
-                await
-                    _paymentSystemsRawLog.RegisterEventAsync(
-                        PaymentSystemRawLogEvent.Create(CashInPaymentSystem.Quanta, "Msg received",
-                            msg.ToJson()));
+                await _paymentSystemsRawLog.RegisterEventAsync(PaymentSystemRawLogEvent.Create(CashInPaymentSystem.Quanta, "Msg received", msg.ToJson()));
 
                 var txId = $"{msg.TransactionHash}_{msg.Contract}";
 
@@ -60,31 +63,33 @@ namespace Lykke.Job.QuantaQueueHandler.TriggerHandlers
 
                 if (pt == null)
                 {
-                    await
-                        _log.WriteWarningAsync(nameof(QuantaQueueHandler), nameof(ProcessInMessage), msg.ToJson(),
-                            "Transaction already handled");
-                    //return if was handled previously
+                    await _log.WriteWarningAsync(nameof(QuantaQueueHandler), nameof(ProcessInMessage), msg.ToJson(), "Transaction already handled");
+                    
+                    _healthService.TraceMessageProcessingFailed();
+
                     return;
                 }
 
-                var result = await
-                    _exchangeOperationsService.IssueAsync(walletCreds.ClientId, LykkeConstants.QuantaAssetId,
-                        msg.Amount);
+                var result = await _exchangeOperationsService.IssueAsync(walletCreds.ClientId, LykkeConstants.QuantaAssetId, msg.Amount);
 
                 if (!result.IsOk())
                 {
-                    await
-                        _log.WriteWarningAsync(nameof(QuantaQueueHandler), msg.ToJson(), result.ToJson(),
-                            "ME error");
+                    await _log.WriteWarningAsync(nameof(QuantaQueueHandler), msg.ToJson(), result.ToJson(), "ME error");
+
+                    _healthService.TraceMessageProcessingFailed();
+
                     return;
                 }
 
-                await
-                    _paymentTransactionsRepository.SetAsOkAsync(pt.Id, msg.Amount, null);
+                await _paymentTransactionsRepository.SetAsOkAsync(pt.Id, msg.Amount, null);
+
+                _healthService.TraceMessageProcessingCompleted();
             }
             catch (Exception ex)
             {
                 await _log.WriteErrorAsync(nameof(QuantaQueueHandler), nameof(ProcessInMessage), msg.ToJson(), ex);
+
+                _healthService.TraceMessageProcessingFailed();
             }
             finally
             {
