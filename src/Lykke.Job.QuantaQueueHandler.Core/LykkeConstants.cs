77133c5885e072950af7d82ce8393326046cf2fb
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2017-07-10 23:59:52 +0300

    Version fixed

--- src/Lykke.Job.QuantaQueueHandler.Core/LykkeConstants.cs
+++ src/Lykke.Job.QuantaQueueHandler.Core/LykkeConstants.cs
@@ -2,10 +2,6 @@
 {
     public static class LykkeConstants
     {
-        public const string BitcoinAssetId = "BTC";
-
-        public const string SolarAssetId = "SLR";
-        public const string ChronoBankAssetId = "TIME";
         public const string QuantaAssetId = "QNT";
     }
 }
